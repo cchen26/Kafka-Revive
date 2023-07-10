94fa977a0dcd9163ec25db44737f802618009c12
Chao Chen <chaochen234@gmail.com>
2023-07-10 16:01:52 -0400

    added configuration for tsconfig

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { KafkaProducerWrapper } from "./ProducerWrapper";
+import { KafkaDLQManager } from "./DLQManager";
+import { KafkaConsumerWrapper } from "./ConsumerWrapper";
+import { KafkaClientWrapper } from "./ClientWrapper";
+import { ErrorHandling } from "./ErrorHandling";
+
+export {
+  KafkaProducerWrapper,
+  KafkaDLQManager,
+  KafkaConsumerWrapper,
+  KafkaClientWrapper,
+  ErrorHandling,
+};
